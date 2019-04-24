98564a45dde639b410d2ded66a49bc61e4aad409
jahskee <jahskee@yahoo.com>
2019-04-24 06:35:02 -0400

    update

---
+++ CustomerMO+CoreDataClass.m
@@ -0,0 +1,14 @@
+//
+//  CustomerMO+CoreDataClass.m
+//  ios-core-data
+//
+//  Created by Mac on 4/24/19.
+//  Copyright © 2019 senapps. All rights reserved.
+//
+//
+
+#import "CustomerMO+CoreDataClass.h"
+
+@implementation CustomerMO
+
+@end
